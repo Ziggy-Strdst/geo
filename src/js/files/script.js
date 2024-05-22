c63d1b190a75420ef9890aae4ca614c98d770b74
Ziggy-Strdst <vadimpolovinko89@gmail.com>
2024-05-22 13:36:12 +0300

    advantages start

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,30 @@
 // Подключение функционала "Чертоги Фрилансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js';
+
+// перенос кнопки бургера
+
+const headerBody = document.querySelector('.header__body');
+const burgerBtn = document.querySelector('.menu__icon');
+const menuLabel = document.querySelector('.header__menu-label');
+
+if (burgerBtn) {
+  burgerBtn.addEventListener('click', function () {
+    headerBody.classList.toggle('header__body-reverse');
+    menuLabel.classList.toggle('hide');
+  });
+}
+
+// mobile menu
+const dropdown = document.querySelectorAll('.dropdown');
+
+if (dropdown) {
+  dropdown.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      link.classList.toggle('dropdown-active');
+      link.children[1].classList.toggle('sub-active');
+    });
+  });
+}
