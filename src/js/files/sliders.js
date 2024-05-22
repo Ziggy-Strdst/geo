85181bc8dd4abb5a141a4f29c28f8b41a8b71868
Ziggy-Strdst <vadimpolovinko89@gmail.com>
2024-05-22 17:44:21 +0300

    main-slider done

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -107,6 +107,78 @@ function initSliders() {
       on: {},
     });
   }
+  if (document.querySelector('.main-slider__slider')) {
+    //Указываем класс нужного слайдера
+    //Создаем слайдер
+    new Swiper('.main-slider__slider', {
+      //Указываем класс нужного слайдера
+      //Подключаем модули слайдера
+      //для конкретного случая
+      modules: [Navigation, Pagination],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 1,
+      spaceBetween: 24,
+      // centeredSlides: true,
+      // initialSlide: 1,
+      //autoHeight: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      //loop: true,
+      //preloadImages: false,
+      //lazy: true,
+
+      /*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+      // Пагинация
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+      },
+
+      // Брейкпоинты
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+          autoHeight: true,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        992: {
+          slidesPerView: 1.2,
+          spaceBetween: 24,
+          centeredSlides: true,
+          initialSlide: 1,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'fraction',
+          },
+        },
+      },
+
+      // События
+      on: {},
+    });
+  }
 }
 //Скролл на базе слайдера (по классу swiper scroll для оболочки слайдера)
 function initSlidersScroll() {
